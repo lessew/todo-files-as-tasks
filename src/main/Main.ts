6861bdfc86ac48ea4d4f8d98c54d9d5ee3dbcfdd
Wessel Heringa <wessel@wesbites.com>
2024-03-18 08:47:53 +0100

    remove async, not needed

--- src/main/Main.ts
+++ src/main/Main.ts
@@ -11,7 +11,7 @@ import { FolderList } from "./obsidian/FolderList";
 
 
 export class Main{
-    static async run(source:string,el:HTMLElement,app:App):Promise<void>{
+    static  run(source:string,el:HTMLElement,app:App):void{
 
         const parser:YAMLParser = new YAMLParser(source);
         const rootPath:string = parser.parseRootPath();
@@ -21,23 +21,20 @@ export class Main{
 
         // load files and folders from obsidian / filesystem
         const folderList = new FolderList();
-        await folderList.init(rootPath);
+        folderList.init(rootPath);
 
         const fileList = new FileList();
-        await fileList.init(rootPath,folderList);
+        fileList.init(rootPath,folderList);
       
         const action = parser.parseAction();
         if(action==YAMLParser.ACTION_LIST){
-            //setTimeout(() => {
-                const properties = TaskFactory.getProperties(rootPath,folderList);
-                const filters = parser.parseFilters(properties);
-                const files = fileList.files;
-                const taskBuilder = new TaskBuilder(files);
-                const filteredFiles = taskBuilder.bulkFilterBy(filters).get();
-                const view = new ObsidianTaskListView(filteredFiles,ObsidianWrapper.getInstance().obsidianApp);
-                view.build(el);
-           // },1250)
-            
+            const properties = TaskFactory.getProperties(rootPath,folderList);
+            const filters = parser.parseFilters(properties);
+            const files = fileList.files;
+            const taskBuilder = new TaskBuilder(files);
+            const filteredFiles = taskBuilder.bulkFilterBy(filters).get();
+            const view = new ObsidianTaskListView(filteredFiles,ObsidianWrapper.getInstance().obsidianApp);
+            view.build(el);
         }
     }
 }
