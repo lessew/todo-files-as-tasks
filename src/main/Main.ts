1f453c816b0dcb67858187a226583bc8f2ce29cc
Wessel Heringa <wessel@wesbites.com>
2024-03-28 10:03:30 +0100

    split into two functions

--- src/main/Main.ts
+++ src/main/Main.ts
@@ -24,22 +24,30 @@ export class Main{
         const folderList = new FolderList();
         folderList.init(rootPath);
 
-        const fileList = new FileList();
-        fileList.init(rootPath,settings,folderList);
-      
         const action = parser.parseAction();
         if(action==YAMLParser.ACTION_LIST){
-            const properties = Task.getProperties(rootPath,settings,folderList);
-            const filters = parser.parseFilters(properties);
-            const files = fileList.files;
-            const fileFilter = new FileFilter(files);
-            const filteredFiles = fileFilter.bulkFilterBy(filters).get();
-            const view = new TaskListView(filteredFiles,ObsidianWrapper.getInstance().obsidianApp);
-            view.build(el);
+           Main.displayActionList(el,parser,rootPath,settings,folderList)
         }
         else if(action==YAMLParser.ACTION_CREATE_BUTTON){
-            const view = new CreateTaskButtonView(app,folderList);
-            view.build(el);
+           Main.displayCreateTaskButton(el,app,folderList)
         }
+    }
+
+    static displayActionList(el:HTMLElement,parser:YAMLParser,rootPath:string,settings:FATPluginSettings,folderList:FolderList):void{
+        const fileList = new FileList();
+        fileList.init(rootPath,settings,folderList);
+
+        const properties = Task.getProperties(rootPath,settings,folderList);
+        const filters = parser.parseFilters(properties);
+        const files = fileList.files;
+        const fileFilter = new FileFilter(files);
+        const filteredFiles = fileFilter.bulkFilterBy(filters).get();
+        const view = new TaskListView(filteredFiles,ObsidianWrapper.getInstance().obsidianApp);
+        view.build(el);
+    }
+
+    static displayCreateTaskButton(el:HTMLElement,obsidianApp:App,folderList:FolderList):void{
+        const view = new CreateTaskButtonView(obsidianApp,folderList);
+        view.build(el);
     }
 }
