28a6818e4bb378c3fdeb4049b17c749a163f3259
Wessel Heringa <wessel@wesbites.com>
2024-04-09 16:28:23 +0200

    now suppers nested folders

--- src/main/obsidian/FolderList.ts
+++ src/main/obsidian/FolderList.ts
@@ -1,29 +1,30 @@
 import { FolderListDAO } from "src/core/Interfaces/FolderListDAO";
 import { ObsidianWrapper } from "./ObsidianWrapper";
-import { TFolder } from "obsidian";
+import { App, TFolder } from "obsidian";
 
 export class FolderList implements FolderListDAO{
     folders:string[];
     
-    // TODO account for case with nested folders.
     // TODO create Folder Interface to allow further abstraction from obsidian code
     // TODO create filesystem wrapper which is implemented by obsidianwrapper
      init(rootPath:string):void{
-        let result:string[] = [];
         const wrapper = ObsidianWrapper.getInstance();
-        const rootFolder = wrapper.getTFolder(rootPath);
-        if(rootFolder instanceof TFolder){
-            rootFolder.children.forEach(child => {
-                if(child instanceof TFolder){
-                    result.push(child.name);
-                }
-            });
-        }
-        else{
-            console.log(`error: given rootpath is not a folder: ${rootPath} - ${rootFolder}`)
-            console.log(wrapper);
-        }
-        this.folders = result;
+        const folders:string[] = FolderList.getFolders(rootPath,wrapper);
+        this.folders = folders;
     }
 
+    static getFolders(path:string,wrapper:ObsidianWrapper):string[]{
+        const folder = wrapper.getTFolder(path);
+        let result:string[] = [];
+        
+        if(folder instanceof TFolder){
+            folder.children.forEach(child => {
+                if(child instanceof TFolder){
+                    result.push(child.name);
+                    result = [...result,...FolderList.getFolders(child.path,wrapper)];
+                }
+            });
+        }       
+        return result; 
+    }
 }
