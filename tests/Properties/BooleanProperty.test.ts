557bf7bfbb694c324ec27a89ecb321a0bf556089
Wessel Heringa <wessel@wesbites.com>
2024-04-20 21:30:32 +0200

    wrote tests that fail

--- tests/Properties/BooleanProperty.test.ts
+++ tests/Properties/BooleanProperty.test.ts
@@ -1,121 +1,42 @@
-import { BooleanYAMLProperty } from "src/core/Properties/BooleanYAMLProperty";
+import { FileModel } from "src/core/File";
+import { BooleanYAMLProperty } from "../../src/core/Properties/BooleanYAMLProperty";
+import { Whitelist } from "../../src/core/Whitelist";
+import { MockFileModel } from "../../tests/Mocks/MockFileModel";
 
 
 class Helper{
   static getBooleanProperty():BooleanYAMLProperty{
-     // return new BooleanYAMLProperty(,settings,file)
+      return new BooleanYAMLProperty(
+        "starred",
+        "true",
+      new Whitelist(["true","false"]),
+      new MockFileModel("path",{starred:"false"}));
   }
 }
-
-/*
-import { BooleanProperty } from "../../src/core/Properties/BooleanProperty";
-import { PropertyModel } from "../../src/core/Interfaces/PropertyModel";
-import { MockPropertyModel } from "../../tests/Mocks/MockPropertyModel";
-
-class Helper{
-  static getProperty(initialValue:string,options:string[]):BooleanProperty{
-    let dao:PropertyModel = new MockPropertyModel(initialValue);
-    let prop = new BooleanProperty("flagged","dummyfileid",dao,{allowedValues:options,defaultValue:""});
-    return prop;
-  }
-}
-
-// TODO: test defaultvalue behaviour
 
 describe('BooleanProperty test correct input', () => {
-  let prop = Helper.getProperty("true",["true","false"]);
+  let prop = Helper.getBooleanProperty();
 
   test('correct property value', () => {
-      expect(prop.getValue()).toBe("true");
+      expect(prop.getValue()).toBe("false");
   });
+  test("setter",()=>{
+      prop.setValue("true")
+      expect(prop.getValue()).toBe("true")
+    })
+    test("setter to invalid value",()=>{
+      prop.setValue("invalid")
+      expect(prop.getValue()).toBe("true")
+    }) 
 });
 
-describe('BooleanProperty test incorrect input:more than 2 values', () => {
-  let options = ["true","false","athird"];
-  let dao:PropertyModel = new MockPropertyModel("true");
-
-  test('incorrect property value should throw error', () => {
-    try{
-      let prop = new BooleanProperty("flagged","dummyfileid",dao,{allowedValues:options,defaultValue:""});
-      expect(true).toBe(false);
-    }
-    catch(e){
-      expect(true).toBe(true)
-    }
-  });
-});
 
 describe('BooleanProperty test toggle', () => {
-  let prop = Helper.getProperty("true",["true","false"]);
+  let prop = Helper.getBooleanProperty();
 
   test('toggle once', () => {
-    prop.toggle();
-    expect(prop.getValue()).toBe("false");
-  });
-  test('toggle twice', () => {
-    prop.toggle();
-    expect(prop.getValue()).toBe("true");
-  });
-  test('toggle thrice', () => {
-    prop.toggle();
-    expect(prop.getValue()).toBe("false");
-  });
-
+    let newval = prop.getNewToggleValue(prop.getValue());
+    expect(newval).toBe("false");
+  }); 
 });
 
-import { PropertySettings } from "../../../src/core/PropertySettings";
-import { BooleanStrategy } from "../../../src/core/PropertyStrategies/YAMLStrategies/BooleanStrategy";
-
-class Helper{
-    static getBooleanStrategy():BooleanStrategy{
-        const settings = {
-            allowedValues:["true","false"],
-            defaultValue:"true"
-        }as PropertySettings
-
-        return new BooleanStrategy(settings);
-    }
-}
-
-describe('booleanstrategy:toggle', () => {
-    let strat = Helper.getBooleanStrategy();
-
-    test('correct values', () => {
-        expect(strat.getNewToggleValue("true")).toBe("false");
-        expect(strat.getNewToggleValue("false")).toBe("true");
-    });  
-    test('incorrect values', () =>{
-        expect(strat.getNewToggleValue("zero")).toBe("true");
-        expect(strat.getNewToggleValue("")).toBe("true");
-    })
-    test('should throw error: invalid default value', () =>{
-        const invalidDefaultValue = {
-            allowedValues:["true","false"],
-            defaultValue:"none"
-        }
-        try{
-            new BooleanStrategy(invalidDefaultValue);
-            expect(true).toBe(false);
-        }
-        catch(e){
-            expect(true).toBe(true)
-        }
-    });
-
-    test('should throw error: invalid allowed values', () =>{
-        const invalidAllowedValues = {
-            allowedValues:["true","false","shouldnotbehere"],
-            defaultValue:"true"
-        }
-        try{
-            new BooleanStrategy(invalidAllowedValues);
-            expect(true).toBe(false);
-        }
-        catch(e){
-            expect(true).toBe(true)
-        }
-
-
-    })
-}); 
-*/
