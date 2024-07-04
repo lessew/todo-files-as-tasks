41cca7567dda084521aa68665c8ad1d4cac36b8e
Wessel Heringa <wessel@wesbites.com>
2024-07-04 16:41:27 +0200

    added codeblocklist

---
+++ src/List/ui/CodeBlockList.ts
@@ -0,0 +1,30 @@
+import FileAsTaskPlugin from "main";
+import { PluginSettings } from "src/Configuration/PluginSettings";
+import { PathPropertyHelper } from "src/Properties/PathPropertyHelper";
+import { PropertyViewFactory } from "src/Properties/PropertyViewFactory";
+import { FileAsTaskCollection } from "../FileAsTaskCollection";
+import { TaskListView } from "./TaskListView";
+
+export class CodeBlockList {
+	view: TaskListView;
+
+	constructor(
+		fatc: FileAsTaskCollection,
+		pluginSettings: PluginSettings,
+		pathPropertyHelper: PathPropertyHelper,
+		plugin: FileAsTaskPlugin,
+		pvf: PropertyViewFactory
+	) {
+		this.view = new TaskListView(
+			fatc,
+			pluginSettings,
+			pathPropertyHelper,
+			plugin,
+			pvf
+		);
+	}
+
+	build(el: HTMLElement) {
+		this.view.build(el);
+	}
+}
